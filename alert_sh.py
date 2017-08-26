9f60ecd19d638f4d8c5e1f0e616099cdba1639a8
Louis-He <1726110778@qq.com>
2017-08-26 19:49:30 +0800

    improve alert_sh.py. It can now map Shanghai. Realtime alert performance is still under construction.

--- alert_sh.py
+++ alert_sh.py
@@ -10,19 +10,21 @@ import matplotlib.lines as mlines
 import numpy as np
 
 # ============================================initialize the plot
-plt.figure(figsize=(13, 9), dpi=120)
+plt.figure(figsize=(5, 6), dpi=120)
 axes = plt.subplot(111)
+
 # set up map projection with
 # use low resolution coastlines.
-map = Basemap(llcrnrlon=119, llcrnrlat=28, urcrnrlon=122, urcrnrlat=32, \
+map = Basemap(llcrnrlon=120.8, llcrnrlat=30.5, urcrnrlon=122.2, urcrnrlat=32, \
               rsphere=(6378137.00, 6356752.3142), \
-              resolution='l', projection='merc', \
+              resolution='h', projection='merc', \
               lat_0=40., lon_0=-20., lat_ts=20.)
+
 # draw coastlines, country boundaries, fill continents.
-map.drawcoastlines(linewidth=0.25)
-map.drawcountries(linewidth=0.25)
+#map.drawcoastlines(linewidth=0.25)
+#map.drawcountries(linewidth=0.25)
 # draw the edge of the map projection region (the projection limb)
-map.drawmapboundary(fill_color='#87CEFA')#689CD2
+map.drawmapboundary(fill_color='#DDDDDD')#689CD2
 # draw lat/lon grid lines every 30 degrees.
 
 #map.drawmeridians(np.arange(0, 360, 10))
@@ -31,4 +33,9 @@ map.drawmeridians(np.arange(0, 360, 10),labels=[0,0,0,1],fontsize=10)
 map.drawparallels(np.arange(-90, 90, 10),labels=[1,0,0,0],fontsize=10)
 
 # Fill continent wit a different color
-map.fillcontinents(color='#FFFFFF', lake_color='#87CEFA', zorder=0)
+map.fillcontinents(color='#DDDDDD', lake_color='#87CEFA', zorder=0)
+
+map.readshapefile(shapefile='/Users/hsw/Downloads/shanghai_shp/Shanghai_county',name='1', drawbounds=True, linewidth=0.5, color='red', default_encoding='UTF-8')
+
+plt.title('上海市实时分区预警信号分布图')
+plt.show()
